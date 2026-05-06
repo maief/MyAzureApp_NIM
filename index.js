9a79676babe31a9955f21f04b513b7046d0c0261
maief <84765731+maief@users.noreply.github.com>
2026-05-06 16:01:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
   res.send("Hello World dari Azure 🚀");
